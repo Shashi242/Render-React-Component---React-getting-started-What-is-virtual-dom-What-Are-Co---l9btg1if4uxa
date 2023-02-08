bad044eeed68c64d6676b7f189f5e9bd0a417b9d
Shashi Raj <84275426+Shashi242@users.noreply.github.com>
2023-02-08 17:36:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-const App = () => {
+class App extends Component {
+  render() {
   return (
     <div id="main">
      <p>I am learning React. My life is getting better.</p>
     </div>
   )
+ }
 }
 
 
